1ec0213e479d34d9116d113d7e37d890cf30deda
costaskk <costas91gr@gmail.com>
2018-06-26 16:22:54 +0300

    Fixed some indentation

--- client/js/submitEnter.js
+++ client/js/submitEnter.js
@@ -3,18 +3,18 @@ window.submitEnter = function() {
     var input = document.getElementById("text");
     // Execute a function when the user releases a key on the keyboard
     input.addEventListener("keyup", function(event) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    //Stop event from firing multiple times
-    event.stopImmediatePropagation();
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Trigger the button element with a click
-        document.getElementById("execute").click();
-        //Remove text from textarea after submit
-        document.getElementById('text').value='';
-    }
-});
+        // Cancel the default action, if needed
+        event.preventDefault();
+        //Stop event from firing multiple times
+        event.stopImmediatePropagation();
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Trigger the button element with a click
+            document.getElementById("execute").click();
+            //Remove text from textarea after submit
+            document.getElementById('text').value='';
+        }
+    });
 }
 
 window.onclick = submitEnter;
