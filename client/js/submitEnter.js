418c975128a46e2cba8e072223766e271d7eda33
costaskk <costas91gr@gmail.com>
2018-06-26 17:23:56 +0300

    Fixed eventHandler error

--- client/js/submitEnter.js
+++ client/js/submitEnter.js
@@ -1,20 +1,22 @@
-window.submitEnter = function() {	
-    // Get the input field
-    var input = document.getElementById("text");
-    // Execute a function when the user releases a key on the keyboard
-    input.addEventListener("keyup", function(event) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        //Stop event from firing multiple times
-        event.stopImmediatePropagation();
-        // Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13) {
-            // Trigger the button element with a click
-            document.getElementById("execute").click();
-            //Remove text from textarea after submit
-            document.getElementById('text').value='';
-        }
-    });
+window.submitEnter = function() {
+    if (document.getElementById("execute")){	
+        // Get the input field
+        var input = document.getElementById("text");
+        // Execute a function when the user releases a key on the keyboard
+        input.addEventListener("keyup", function(event) {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            //Stop event from firing multiple times
+            event.stopImmediatePropagation();
+            // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13) {
+                // Trigger the button element with a click
+                document.getElementById("execute").click();
+                //Remove text from textarea after submit
+                document.getElementById('text').value='';
+            }
+        });
+    }
 }
 
 window.onclick = submitEnter;
