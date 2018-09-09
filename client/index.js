c179af1dabc0e3e005a97ed103bd0e8f6ccc5efa
costaskk <costas91gr@gmail.com>
2018-09-09 22:07:14 +0300

    Fixed several issues with exercises

--- client/index.js
+++ client/index.js
@@ -47,10 +47,22 @@ $.get('/menu_items', function(data) {
 }).done(function(){
     var insert="";
     for (var i in json) {
-        insert += "<li><a href=categories="+json[i].link+">"+json[i].name+"</a></li>"
+        if (json[i].isParent === false) {
+            insert += "<li><a href=categories=" + json[i].link + ">" + json[i].name + "</a></li>";
+        } 
+        else {
+            insert += "<a href=\"" + json[i].link + "\" data-toggle=\"collapse\" aria-expanded=\"false\">" + json[i].name + "</a>";
+            insert += "<ul class=\"collapse list-unstyled\" id=\"" + json[i].link.replace("#", "") + "\">";
+
+            for (var j in json[i].subItems) {
+                insert += "<li><a href=categories=" + json[i].subItems[j].link + ">" + json[i].subItems[j].name + "</a></li>";
+            }
+
+            insert += "</ul>";
+        }
     }
     var add = document.getElementById("about");
-    add.insertAdjacentHTML("beforebegin", insert);
+    add.insertAdjacentHTML("afterbegin", insert);
 });
 
 
@@ -84,57 +96,85 @@ else if (location.pathname == '/categories='+category) {
         contents = data;
     }).done(function(){
         var test="";
+        var queryProblemsHTML;
+        $.get('query_problems.html', function (data) {
+            queryProblemsHTML = data;
 
-        for (var i in contents) {
-            // If a header exists in mongoDB then add it to the top of the page
-            if (contents[i].header) {
-                test += "<h1 align='center'>"+contents[i].header+"<h1><hr style='width:40%;'><br/>";
+            for (var i in contents) {
+                var chapterText = "";
+                // If a header exists in mongoDB then add it to the top of the page
+                if (contents[i].header) {
+                    chapterText += "<h1 align='center'>"+contents[i].header+"<h1><hr style='width:40%;'><br/>";
+                }
+                //Fill the page with the contents from the collection in mongoDB
+                chapterText += "<h2>" + contents[i].title + "</h2>" + contents[i].text + "<br/><br/>";
+                
+                var contentElement = document.getElementById("content");
+                contentElement.insertAdjacentHTML("beforeend", chapterText);
+                // var exec = document.getElementById("content");
+                // exec.insertAdjacentHTML("beforeend", chapterText);
+                
+
+                if (contents[i].hasProblemQuery == 'true') {
+                    var solution = contents[i].query;
+                    console.log(solution);
+                    var hiddenField = "hiddenField" + contents[i].order;
+                    var execute = "execute" + contents[i].order;
+                    var feedback = "feedback" + contents[i].order;
+                    var text = "text" + contents[i].order;
+                    var exists = "exists" + contents[i].order;
+                    var result = "result" + contents[i].order;
+
+                    contentElement.insertAdjacentHTML("beforeend", queryProblemsHTML);
+
+                    var hiddenElement = document.getElementById("hiddenField");
+                    hiddenElement.setAttribute("value", solution);
+                    hiddenElement.setAttribute("id", hiddenField);
+
+                    var hiddenElement = document.getElementById("execute");
+                    hiddenElement.setAttribute("onclick", "problemCheck(" + contents[i].order + ");");
+                    hiddenElement.setAttribute("id", execute);
+
+                    var hiddenElement = document.getElementById("feedback");
+                    hiddenElement.setAttribute("id",feedback);
+
+                    var hiddenElement = document.getElementById("text");
+                    hiddenElement.setAttribute("id",text);
+
+                    var hiddenElement = document.getElementById("exists");
+                    hiddenElement.setAttribute("id",exists);
+
+                    var hiddenElement = document.getElementById("result");
+                    hiddenElement.setAttribute("id",result);
+                }
+                //Check if execute query is used, if yes then include execute_query.html which includes the query run
+                else if (contents[i].hasExecuteQuery == 'true') {    
+                    
+                    //Fill query input with a default query stored in mongoDB
+                    var queryInsert = contents[i].query;
+
+                    //Initiate query variable to store html data
+                    var query;
+
+                    //Include execute_query.html in our page
+                    $.get('execute_query.html', function(data) {
+                        query=data;
+                    }).done(function(){
+                        //Insert execute_query.html elements into page
+                        var exec = document.getElementById("content");
+                        exec.insertAdjacentHTML("beforeend", query);
+
+                        //After execute_query.html elements have been inserted insert default query to input with id text
+                        document.getElementById("text").value= queryInsert;
+
+                        //When page which includes execute_query.html loads, run function tableList() to create a table with a list of tables in sqlite
+                        tableList();
+                    });
+                }
+                var line = "<div class='line'></div>";
+                var lineElement = document.getElementById("content");
+                lineElement.insertAdjacentHTML("beforeend", line);
             }
-            //Fill the page with the contents from the collection in mongoDB
-            test += "<h2>"+contents[i].title+"</h2>"+contents[i].text+"<div class='line'></div>";
-        }
-        
-        var elements = document.getElementById("content");
-        elements.insertAdjacentHTML("beforeend", test);
-
-        //Check if execute query is used, if yes then include execute_query.html which includes the query run
-        if (contents[i].hasExecuteQuery == 'true') {    
-            
-            //Fill query input with a default query stored in mongoDB
-            var queryInsert = contents[i].query;
-
-            //Initiate query variable to store html data
-            var query;
-
-            //Include execute_query.html in our page
-            $.get('execute_query.html', function(data) {
-                query=data;
-            }).done(function(){
-                //Insert execute_query.html elements into page
-                var exec = document.getElementById("content");
-                exec.insertAdjacentHTML("beforeend", query);
-
-                //After execute_query.html elements have been inserted insert default query to input with id text
-                document.getElementById("text").value= queryInsert;
-
-                //When page which includes execute_query.html loads, run function tableList() to create a table with a list of tables in sqlite
-                tableList();
-            });
-        } 
-        else if (contents[i].hasProblemQuery == 'true') {
-
-            //Initiate query variable to store html data
-            var query;
-            var solution = contents[i].query;
-            //Include execute_query.html in our page
-            $.get('query_problems.html', function(data) {
-                query=data;
-            }).done(function(){
-                //Insert execute_query.html elements into page
-                var exec = document.getElementById("content");
-                exec.insertAdjacentHTML("beforeend", query);
-                document.getElementById("hidden").value= solution;
-            });
-        }
+        });
     });
 }
--- client/js/problemCheck.js
+++ client/js/problemCheck.js
@@ -1,41 +1,76 @@
 import {db} from './initializeDB';
 
-window.problemCheck = function() {
+window.problemCheck = function(order) {
+
+
     //Get table names from database
-    var text = document.getElementById("text").value;
+    var text = document.getElementById("text" + order).value;
     try {
-        //var correct_query = "SELECT * FROM users";
-        var correct = db.exec(document.getElementById("hidden").value);
-        //Execute query and store into variable query_results
-        var query_results = db.exec(text);
-        console.log(document.getElementById("hidden").value);
-        //Remove ; from end of user query, if it exists
-        if (text.slice(-1)==';') {
-            text=text.slice(0,-1);
+        var firstWord = text.split(' ');
+        if (firstWord[0].toUpperCase() == "SELECT") {
+            var correct = db.exec(document.getElementById("hiddenField" + order).value);
+            //Execute query and store into variable query_results
+            var query_results = db.exec(text);
+            //console.log(document.getElementById("hiddenField"+order).value);
+            //Remove ; from end of user query, if it exists
+            if (text.slice(-1)==';') {
+                text=text.slice(0,-1);
+            }
+
+            var select_results = db.exec(text);
+        }
+        else {
+            var correct = document.getElementById("hiddenField" + order).value;
+            var query_results = text;
+
+            if (query_results.slice(-1)==';') {
+                query_results=query_results.slice(0,-1);
+            }
+
+            if (correct.slice(-1)==';') {
+                correct=correct.slice(0,-1);
+            }
+
+            db.exec(text);
+            //if ((firstWord[0].toUpperCase() != "CREATE")&&(firstWord[0].toUpperCase() != "DROP")) {
+                var select_results = db.exec("SELECT * FROM users");
+            //}
         }
 
        //Store results into a table and display it
         if (_.isEqual(query_results,correct)) {
             //Remove text from textarea after submit
-            document.getElementById('text').value='';
-            if (text.trim().length>0) {
-                //Helpful message
-                $('#feedback').show();
-                var table_string = 'Your query is correct, here is the result<br/><br/>'; 
-                document.getElementById("feedback").innerHTML = table_string; 
+            document.getElementById('text'+order).value='';
 
-                $('#exists').show();
+            //Helpful message
+            $('#feedback'+order).show();
+            var table_string = 'Your query is correct!<br/><br/>'; 
+            document.getElementById("feedback"+order).innerHTML = table_string; 
 
-                //Remove ; character from string
-                var string = text.replace(';','');
+            $('#exists'+order).show();
 
+            //Remove ; character from string
+            var string = text.replace(';','');     
+        
+            //  //Datatable Code
+            //  var dataSet = [];
+            //  var queryColumns = [{title: 'Table Name'}, {title: 'Number of Entries'}];
+            //  var results = select_results[0].values;
+
+            //  for (var index in results) {
+            //      var tableName = results[index][0];
+            //      var numberOfEntries = db.exec("SELECT COUNT() FROM " + tableName + ";")[0].values;
+
+            //      dataSet.push([tableName, numberOfEntries]);
+            //  }
+        
+            if ((firstWord[0].toUpperCase() != "CREATE")&&(firstWord[0].toUpperCase() != "DROP")) { 
+                var queryColumns = select_results[0].columns;
+                var results = select_results[0].values;
+                
                 //Datatable Code
                 var columns = [];
-                var dataSet = [];
-
-                var queryColumns = query_results[0].columns;
-                var results = query_results[0].values;
-
+                var dataSet = [];;
                 for (var index in queryColumns) {
                     var tableName = queryColumns[index];
                     columns.push({title: tableName});
@@ -46,13 +81,13 @@ window.problemCheck = function() {
                 }
 
                 var table;
-                
-                if ($.fn.DataTable.isDataTable('#result')) {
-                    $('#result').DataTable().destroy();
-                    $('#result').empty();
+            
+                if ($.fn.DataTable.isDataTable('#result'+order)) {
+                    $('#result'+order).DataTable().destroy();
+                    $('#result'+order).empty();
                 };
 
-                table = $('#result').DataTable( {
+                table = $('#result'+order).DataTable( {
                     data: dataSet,
                     columns: columns,
                     destroy : true
@@ -60,23 +95,23 @@ window.problemCheck = function() {
             }
         }
         else {
-            $('#exists').hide();
-            $('#feedback').show();
+            $('#exists'+order).hide();
+            $('#feedback'+order).show();
             var table_string = 'Your query does not have the intended result'; 
-            document.getElementById("feedback").innerHTML = table_string; 
+            document.getElementById("feedback"+order).innerHTML = table_string; 
         }
     }
     catch(e) {
-        $('#exists').hide();
-        $('#feedback').show();
+        $('#exists'+order).hide();
+        $('#feedback'+order).show();
         //If table is empty error
         if (e.message == 'Cannot read property \'columns\' of undefined') {
             var table_string = 'Table '+word3+' is empty'; 
-            document.getElementById("feedback").innerHTML = table_string; 
+            document.getElementById("feedback"+order).innerHTML = table_string; 
         }
         //Other errors
         else {
-            document.getElementById("feedback").innerHTML = e.message;
+            document.getElementById("feedback"+order).innerHTML = e.message;
         }
     }
 }
